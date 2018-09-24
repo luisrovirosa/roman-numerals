0bc2f12790392d833efd8753c3e6de6875f036a0
Luis Rovirosa <luisrovirosa@gmail.com>
2018-09-24 10:58:46 +0200

    refactor extract list of conversions outside the method toRoman

--- src/main/java/com/luisrovirosa/roman_numerals/Converter.java
+++ src/main/java/com/luisrovirosa/roman_numerals/Converter.java
@@ -6,27 +6,29 @@ import java.util.Map;
 
 public class Converter {
     public String toRoman(int number) {
-        HashMap<Integer, String> letters = new LinkedHashMap<Integer, String>() {{
-            put(1000, "M");
-            put(900, "CM");
-            put(500, "D");
-            put(400, "CD");
-            put(100, "C");
-            put(90, "XC");
-            put(50, "L");
-            put(40, "XL");
-            put(10, "X");
-            put(9, "IX");
-            put(5, "V");
-            put(4, "IV");
-            put(1, "I");
-        }};
-
-        for (Map.Entry<Integer, String> conversion : letters.entrySet()) {
+        for (Map.Entry<Integer, String> conversion : conversions().entrySet()) {
             if (number >= conversion.getKey()) {
                 return conversion.getValue() + toRoman(number - conversion.getKey());
             }
         }
         return "";
+    }
+
+    private HashMap<Integer, String> conversions() {
+        return new LinkedHashMap<Integer, String>() {{
+                put(1000, "M");
+                put(900, "CM");
+                put(500, "D");
+                put(400, "CD");
+                put(100, "C");
+                put(90, "XC");
+                put(50, "L");
+                put(40, "XL");
+                put(10, "X");
+                put(9, "IX");
+                put(5, "V");
+                put(4, "IV");
+                put(1, "I");
+            }};
     }
 }
