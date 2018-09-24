2db0bbca812cd09402f7e92616e2b5db7484432a
Luis Rovirosa <luisrovirosa@gmail.com>
2018-09-24 10:26:53 +0200

    test refactor reduce duplicity

--- src/test/java/com/luisrovirosa/roman_numerals/ConverterTest.java
+++ src/test/java/com/luisrovirosa/roman_numerals/ConverterTest.java
@@ -1,29 +1,32 @@
 package com.luisrovirosa.roman_numerals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ConverterTest {
+
+    private Converter converter;
+
+    @Before
+    public void setUp(){
+        converter = new Converter();
+    }
+
     @Test
     public void converts_one_into_i(){
-        Converter converter = new Converter();
-
         assertThat(converter.toRoman(1), is("I"));
     }
 
     @Test
     public void converts_two_into_ii(){
-        Converter converter = new Converter();
-
         assertThat(converter.toRoman(2), is("II"));
     }
 
     @Test
     public void converts_three_into_iii(){
-        Converter converter = new Converter();
-
         assertThat(converter.toRoman(3), is("III"));
     }
 }
