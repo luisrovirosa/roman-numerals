f7f4a4fa741a0cd3499efdadd6d04235483df48c
Luis Rovirosa <luisrovirosa@gmail.com>
2018-09-24 10:36:25 +0200

    adds recursion to avoid duplicity

--- src/main/java/com/luisrovirosa/roman_numerals/Converter.java
+++ src/main/java/com/luisrovirosa/roman_numerals/Converter.java
@@ -3,9 +3,11 @@ package com.luisrovirosa.roman_numerals;
 public class Converter {
     public String toRoman(int number) {
         String[] numbers = {
-                "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
-                "XI", "XII", "XIII", "XIV", "XV"
+                "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
         };
+        if (number > 10){
+            return "X" + toRoman(number-10);
+        }
         return numbers[number - 1];
     }
 }
