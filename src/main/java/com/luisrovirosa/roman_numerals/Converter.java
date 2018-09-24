ec195172c3915e69541d94e73c51a0d5dab88501
Luis Rovirosa <luisrovirosa@gmail.com>
2018-09-24 11:16:40 +0200

    refactor to functional style

--- src/main/java/com/luisrovirosa/roman_numerals/Converter.java
+++ src/main/java/com/luisrovirosa/roman_numerals/Converter.java
@@ -3,32 +3,32 @@ package com.luisrovirosa.roman_numerals;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Converter {
     public String toRoman(int number) {
-        for (Map.Entry<Integer, String> conversion : conversions().entrySet()) {
-            if (number >= conversion.getKey()) {
-                return conversion.getValue() + toRoman(number - conversion.getKey());
-            }
+        Optional<Map.Entry<Integer, String>> conversion = conversions().entrySet().stream().filter(c -> number >= c.getKey()).findFirst();
+        if (!conversion.isPresent()) {
+            return "";
         }
-        return "";
+        return conversion.get().getValue() + toRoman(number - conversion.get().getKey());
     }
 
     private HashMap<Integer, String> conversions() {
         return new LinkedHashMap<Integer, String>() {{
-                put(1000, "M");
-                put(900, "CM");
-                put(500, "D");
-                put(400, "CD");
-                put(100, "C");
-                put(90, "XC");
-                put(50, "L");
-                put(40, "XL");
-                put(10, "X");
-                put(9, "IX");
-                put(5, "V");
-                put(4, "IV");
-                put(1, "I");
-            }};
+            put(1000, "M");
+            put(900, "CM");
+            put(500, "D");
+            put(400, "CD");
+            put(100, "C");
+            put(90, "XC");
+            put(50, "L");
+            put(40, "XL");
+            put(10, "X");
+            put(9, "IX");
+            put(5, "V");
+            put(4, "IV");
+            put(1, "I");
+        }};
     }
 }
