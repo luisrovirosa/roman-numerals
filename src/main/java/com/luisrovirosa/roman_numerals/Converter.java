1fb27859867d8871f59a129b5ed5c36ae5501250
Luis Rovirosa <luisrovirosa@gmail.com>
2018-09-24 10:28:25 +0200

    converter simplify

--- src/main/java/com/luisrovirosa/roman_numerals/Converter.java
+++ src/main/java/com/luisrovirosa/roman_numerals/Converter.java
@@ -2,12 +2,7 @@ package com.luisrovirosa.roman_numerals;
 
 public class Converter {
     public String toRoman(int number) {
-        if (number == 2){
-            return "II";
-        }
-        if (number == 3){
-            return "III";
-        }
-        return "I";
+        String[] numbers = {"I", "II", "III"};
+        return numbers[number - 1];
     }
 }
