513a267317e00809bb6a85635daed19a1fea36aa
Luis Rovirosa <luisrovirosa@gmail.com>
2018-09-24 10:37:56 +0200

    adds null object to avoid -1 and special case for 10

--- src/main/java/com/luisrovirosa/roman_numerals/Converter.java
+++ src/main/java/com/luisrovirosa/roman_numerals/Converter.java
@@ -3,11 +3,11 @@ package com.luisrovirosa.roman_numerals;
 public class Converter {
     public String toRoman(int number) {
         String[] numbers = {
-                "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
+                "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
         };
-        if (number > 10){
+        if (number >= 10){
             return "X" + toRoman(number-10);
         }
-        return numbers[number - 1];
+        return numbers[number];
     }
 }
